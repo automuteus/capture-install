2722c8d7c0a22c4a86fd99669b5db8577b102b80
Aiden <58155937+Wolfhound905@users.noreply.github.com>
2021-08-12 01:14:13 -0400

    Updated for 5.0.9 Runtime
    :)

--- CaptureInstaller.bat
+++ CaptureInstaller.bat
@@ -1,10 +1,11 @@
-@Echo Off
-powershell -window hidden -command ""
+@Echo off
+@powershell -window hidden -command ""
 Mode 80,3 & color 0A
 
-Set NETdownloadLink=https://download.visualstudio.microsoft.com/download/pr/c6a74d6b-576c-4ab0-bf55-d46d45610730/f70d2252c9f452c2eb679b8041846466/windowsdesktop-runtime-5.0.1-win-x64.exe
+Set NETdownloadLink=https://download.visualstudio.microsoft.com/download/pr/f3bb58e7-45e1-46ef-9b90-877a450e345e/b18e3d2c429422e9c1238c9b66ded855/dotnet-runtime-5.0.9-win-x64.exe
+
 Set CAPTUREdownloadlink=https://github.com/automuteus/amonguscapture/releases/latest/download/AmongUsCapture.zip
-Set NET5HASH=a7f9fc194371e125de609c709b52b1ac
+Set NET5HASH=fb2312b7ddf859de13d04138396901d9
 Set zipName=AmongUsCapture.zip
 Set captureName=AmongUsCapture.exe
 
@@ -16,7 +17,7 @@ for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1)
 REM -----------
 
 cmd /c "dotnet --list-runtimes" > "%TEMP%\desktopRuntimes.txt"
-find "Microsoft.WindowsDesktop.App 5.0.1" %TEMP%\desktopRuntimes.txt && (
+find "Microsoft.WindowsDesktop.App 5.0.9" %TEMP%\desktopRuntimes.txt && (
   del "%TEMP%\desktopRuntimes.txt"
   REM .Net Runtime Already installed
   goto checkForCapture
@@ -30,7 +31,7 @@ find "Microsoft.WindowsDesktop.App 5.0.1" %TEMP%\desktopRuntimes.txt && (
 REM ―――――― Check Sums
 :checkSumNetRuntime
 set "RECEIVED=" & for /F "skip=1 delims=" %%H in ('
-    2^> nul CertUtil -hashfile "%TEMP%\windowsdesktop-runtime-5.0.1-win-x64.exe" md5
+    2^> nul CertUtil -hashfile "%TEMP%\windowsdesktop-runtime-5.0.9-win-x64.exe" md5
 ') do if not defined RECEIVED set "RECEIVED=%%H"
 if "%NET5HASH%"=="%RECEIVED%" (
     REM Correct hash
@@ -43,33 +44,34 @@ REM ――――――――――――――――――
 
 :installNetRuntime
 cls
-echo off
+@Echo off
 cls
 @powershell -window normal -command ""
 call :colorEcho 0A "        ---Downloading .NET 5 Desktop Runtime Installer (dependency)---"
 echo.
 echo.
-curl -# "%NETdownloadLink%" -o "%TEMP%\windowsdesktop-runtime-5.0.1-win-x64.exe"
+curl -# "%NETdownloadLink%" -o "%TEMP%\windowsdesktop-runtime-5.0.9-win-x64.exe"
 @powershell -window hidden -command ""
 goto checkSumNetRuntime
 
 :launchNetRuntime
-powershell -window hidden -command ""
+@powershell -window hidden -command ""
 
 @REM curl -LJs "https://raw.githubusercontent.com/automuteus/capture-install/main/resetvars.vbs" -o "%TEMP%\resetvars.vbs"
 @REM curl isnt working for some reason, so instead I am using a powershell command that does the same thing
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/automuteus/capture-install/main/resetvars.vbs -Outfile '%TEMP%\resetvars.vbs'"
 
-start "" "%TEMP%\windowsdesktop-runtime-5.0.1-win-x64.exe" /passive /install /norestart
+start "" "%TEMP%\windowsdesktop-runtime-5.0.9-win-x64.exe" /passive /install /norestart
 goto detectIfdoneInstall
 
 :detectIfdoneInstall
 cmd /c "dotnet --list-runtimes" > "%TEMP%\desktopRuntimes.txt"
-find "Microsoft.WindowsDesktop.App 5.0.1" %TEMP%\desktopRuntimes.txt && (
+find "Microsoft.NETCore.App 5.0.9" %TEMP%\desktopRuntimes.txt && (
   REM .NET 5 is done installing
+  echo TEST
   del "%TEMP%\desktopRuntimes.txt"
-  taskkill /im "windowsdesktop-runtime-5.0.1-win-x64.exe" /f
-  del "%TEMP%\windowsdesktop-runtime-5.0.1-win-x64.exe"
+  taskkill /im "windowsdesktop-runtime-5.0.9-win-x64.exe" /f
+  del "%TEMP%\windowsdesktop-runtime-5.0.9-win-x64.exe"
   del "%TEMP%\resetvars.vbs"
   goto checkForCapture
 ) || (
@@ -79,20 +81,21 @@ find "Microsoft.WindowsDesktop.App 5.0.1" %TEMP%\desktopRuntimes.txt && (
   %TEMP%\resetvars.vbs
   call "%TEMP%\CaptureInstaller.bat"
   tasklist.exe | findstr "windowsdesktop-runtime-5." > nul
-  if errorlevel 1 ( 
+  if errorlevel 1 (
     goto EOF
   )
   goto detectIfdoneInstall
 )
 
 :checkForCapture
+echo HI
 if EXIST "%captureName%" ( goto launchCapture )
 if EXIST "%zipName%" ( goto unZip )
 if not EXIST "%captureName%" ( goto installCapture )
 
 :installCapture
 cls
-echo off
+@Echo off
 cls
 @powershell -window normal -command ""
 call :colorEcho 0A "                      ---Downloading AutoMuteUs Capture---"
@@ -103,7 +106,7 @@ goto :unZip
 
 :unZip
 cls
-echo off
+@Echo off
 cls
 @powershell -window normal -command ""
 call :colorEcho 0A "                          ---Inflating the Zip---"
@@ -122,10 +125,11 @@ goto EOF
 
 REM Color Stuff ----
 :colorEcho
-echo off
+@Echo off
 <nul set /p ".=%DEL%" > "%~2"
 findstr /v /a:%1 /R "^$" "%~2" nul
 del "%~2" > nul 2>&1i
 REM --------
+
 
 :EOF
